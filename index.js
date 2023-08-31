a4d19a8194b5a48f83b3f16f9f56118483491d06
Rajendrankr25 <rajendrankr25@gmail.com>
2023-08-31 09:04:53 +0530

    routes and coding implemented

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from "express";
+import * as dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import cors from 'cors';
+import userRouter from './routes/user.route.js';
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 
-const PORT = 4000;
+const MONGO_URL = process.env.MONGO_URL;
+const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log('mongodb is connected');
+
+app.use(express.json());
+app.use(cors());
+
 app.get("/", function (request, response) {
-    response.send("🙋‍♂️, 🌏 🎊✨🤩");
+    response.send("The server is Running");
 });
 
+app.use("/user", userRouter);
+
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+export { client };
